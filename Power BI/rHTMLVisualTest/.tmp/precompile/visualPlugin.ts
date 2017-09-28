f0b62e06b904df0883b14a6d7fdfb4869ac7cab9
William-Ch <william.chuang@brisbane.qld.gov.au>
2017-09-28 19:54:33 +1000

    upload Power BI R html custom visual

---
+++ Power BI/rHTMLVisualTest/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var rHTMLVisualTestE7FF8C6F9855453CAA9EB56891E1C81F = {
+        name: 'rHTMLVisualTestE7FF8C6F9855453CAA9EB56891E1C81F',
+        displayName: 'RHTMLVisualTest',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.7.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.rHTMLVisualTestE7FF8C6F9855453CAA9EB56891E1C81F.Visual(options),
+        custom: true
+    };
+}
